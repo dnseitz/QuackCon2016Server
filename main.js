4150949c5c7d43335d6ab2a07f0cdc8ef184e20a
Daniel Seitz <dnseitz@gmail.com>
2016-10-15 09:16:47 -0700

    git credentials

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const Joi = require('joi');
 const AWS = require('aws-sdk');
 const SNS = new AWS.SNS({region: 'us-west-2'});
 
+AWS.config.loadFromPath('./config.json');
+
 const iosAppArn = "arn:aws:sns:us-west-2:164008979560:app/APNS_SANDBOX/QuackCon2016"
 const andrAppArn = ""
 const errorStrings = {
@@ -106,6 +108,14 @@ server.route({
 		}
 		else if (query.type === 'android') {
 			appArn = andrAppArn;
+		}
+		else {
+			const error = {
+				error: 'UNKNOWN_DEVICE_TYPE',
+				info: [query.type]
+			};
+			reply(error);
+			return;
 		}
 
 		const params = {
