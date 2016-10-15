eb3861a1e1cac3d2e7a6cd8f16e6c77e5c43478e
Daniel Seitz <dnseitz@gmail.com>
2016-10-15 14:00:52 -0700

    publish working with objects

--- main.js
+++ main.js
@@ -34,13 +34,21 @@ server.route({
 
 function publish(message, topicArn, cb) {
 	let isObject = (typeof message !== null && typeof message === 'object')
+
 	var params = {
 		Message: message,
 		TopicArn: topicArn
 	};
 	if (isObject) {
+		let wrapper = {
+			'default': 'default',
+			'APNS': JSON.stringify({'aps': {'content-available': 1, data: message}}),
+			'APNS_SANDBOX': JSON.stringify({'aps': {'content-available': 1, data: message}})
+		}
 		params.MessageStructure = 'json';
+		params.Message = JSON.stringify(wrapper);
 	}
+
 	SNS.publish(params, function(err, data) {
 		if (err) {
 			console.log(err, err.stack);
@@ -281,6 +289,12 @@ server.start((err) => {
 	}
 
 	console.log(`Server running at: ${server.info.uri}`);
+
+	let data = {
+		someData: "SOME DATA",
+		otherData: "Other"
+	};
+	publish(data, "arn:aws:sns:us-west-2:164008979560:HapiTest");
 });
 
 
